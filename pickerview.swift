f8f197837fe101d6724a29b5503cf4831bc888c9
mohamedreda1993 <45246611+mohamedreda1993@users.noreply.github.com>
2019-02-06 21:31:43 +0200

    Create pickerview.swift
    how to use pickerview in xcode

---
+++ pickerview.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  slider
+//
+//  Created by mohamed on 2/6/19.
+//  Copyright © 2019 mohamed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSource {
+    var foods=["orange","apple","banana","tomatos","beans"]
+    @IBOutlet weak var lab: UILabel!
+ 
+    
+   
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return foods.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return foods[row]
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        lab.text=foods[row]
+    }
+
+
+}
